3d381307f75306bb813233fc7cb2fcf00fa7bbec
BeatrizCercal <beatriz_c_cachoeira@estudante.sesisenai.org.br>
2025-08-22 11:57:52 -0300

    exercicio 01 finalizado

---
+++ exercicio01.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 01 - Verificar se o número é par ou ímpar.</title>
+</head>
+
+<body>
+    <h1 style="position: relative; left:10px">Verificar se o número é par ou ímpar.</h1>
+    <form method="POST" action="exercicio01.php" class="m-3">
+            <input type="text" id="num1" class="form-control" name="num1" required>
+            <br>
+            <!--<input type="text" id="num2" class="form-control" name="num2" required>
+            <br>-->
+            <button type="submit" class="">Verifique aqui</button>
+    </form>
+</body>
+</html>
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $num1 = isset($_POST['num1']) ? (int)$_POST['num1'] : 0;
+
+    if ($num1 % 2 == 0) {
+        echo "<p style='position: relative; left:10px'>O número $num1 é par.</p>";
+    } else {
+        echo "<p style='position: relative; left:10px'>O número $num1 é ímpar.</p>";
+    }
+}
+
+?>
