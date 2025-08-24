0a67062257dec7e5794868ca6ab896b892b72487
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2025-08-23 21:31:31 -0300

    att

---
+++ exercicio20.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 20 - Verificação de Senha</title>
+</head>
+
+<body>
+    <h1 style="position: relative; left:10px">Insira uma senha forte para avaliação (Letras e Números)</h1>
+    <form method="POST" action="exercicio17.php" class="m-3">
+            <input type="text" id="password" class="form-control" name="senha" required>
+
+
+            <br>
+            <button type="submit" class="">Verifique Aqui</button>
+    </form>
+</body>
+</html>
