41144e75152d99d7ac5a8f6f1c2b67dcc7e9fc96
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2025-08-23 21:45:39 -0300

    exercicio 4 finalizado

---
+++ exercicio04.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 04 - Fatorial</title>
+</head>
+
+<body>
+    <h1 style="position: relative; left:10px">Verificar o Fatorial de um Número</h1>
+    <form method="POST" action="exercicio04.php" class="m-3">
+            <input type="text" id="num1" class="form-control" name="num1" required>
+            <br>
+            <button type="submit" class="">Verifique Aqui</button>
+    </form>
+</body>
+</html>
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $num1 = isset($_POST['num1']) ? (int)$_POST['num1'] : 0;
+
+    $fatorial = gmp_fact($num1);
+
+    echo "O fatorial de " . $num1 . " é " . gmp_strval($fatorial);
+    
+}  
+
+?>
