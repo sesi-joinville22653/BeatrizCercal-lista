c6a0c1652353360ae10011ca3f7a6d28fc967be6
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2025-08-23 21:02:06 -0300

    exercicio 17 finalizado

--- exercicio16.php
+++ exercicio16.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercicio 14 - Permitido Votar</title>
+    <title>Exercicio 16 - Permitido Votar</title>
 </head>
 
 <body>
