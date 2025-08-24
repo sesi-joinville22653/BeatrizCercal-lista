0f269d5088c07082491cdf97e2c5ceaf6a90717a
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2025-08-23 21:30:26 -0300

    exercicio 19 realizado

--- exercicio11.php
+++ exercicio11.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercicio 10 - Palavra Palíndromo</title>
+    <title>Exercicio 11 - Palavra Palíndromo</title>
 </head>
 
 <body>
---
+++ exercicio19.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 19 - Verificação de Senha</title>
+</head>
+
+<body>
+    <h1 style="position: relative; left:10px">Insira uma senha forte para avaliação (Letras e Números)</h1>
+    <form method="POST" action="exercicio17.php" class="m-3">
+            <input type="text" id="password" class="form-control" name="senha" required>
+
+
+            <br>
+            <button type="submit" class="">Verifique Aqui</button>
+    </form>
+</body>
+</html>
+
+<?php
+ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $senha = $_POST['senha'];
+        $forte = strlen($senha) >= 8 && preg_match('/[A-Za-z]/', $senha) && preg_match('/\d/', $senha);
+        if ($forte) {
+            echo "Senha forte.";
+        } else {
+            echo "Senha fraca.";
+        }
+    }
+?>
